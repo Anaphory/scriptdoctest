947d0dfdf5b9874506de47ea1e1c49bbcb321ea2
Gereon A. Kaiping <g.a.kaiping@hum.leidenuniv.nl>
2018-05-29 14:58:55 +0200

    Increase version number, fix name

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="scriptdocdest",
-    version="0.1",
+    name="scriptdoctest",
+    version="0.2",
     description="Verify interactive shell session examples",
     author="Gereon Kaiping",
     author_email="anaphory@yahoo.de",
